8a5d550969261465925f9e36b8e0dc4cce95db4e
Takuma Aoki <aoking1213@gmail.com>
2023-04-15 15:54:41 +0900

    Vuetifyのインストール

---
+++ components/atoms/IconButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <!--v-btn :color="color" :dark="dark" :fab="fab" :icon="icon" :large="large" :left="left" :right="right" :round="round"
+        :small="small" :tile="tile" :disabled="disabled" @click="onClick">
+        <v-icon>{{ icon }}</v-icon>
+    </v-btn-->
+    <v-btn>あああ</v-btn>
+</template>
+  
