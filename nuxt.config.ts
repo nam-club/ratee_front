9c8280221b50d7b4d0f2dd266495ee8f62cd31da
Takuma Aoki <aoking1213@gmail.com>
2023-04-23 15:36:22 +0900

    VuetifyおよびComposition APIの構築

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,5 +25,6 @@ export default defineNuxtConfig({
         '~/components/atoms',
         '~/components/molecules',
         '~/components/organisms',
-    ]
+    ],
+    plugins: ['~/plugins/vuetify.ts'],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        <IconButton />
+        <!--IconButton /-->
+        <h1>{{ message }}</h1>
+        <v-btn @click="incrementCounter">カウンター: {{ counter }}</v-btn>
     </div>
 </template>
+
+<script>
+import { ref, computed } from 'vue'
+
+export default {
+    setup() {
+        const message = ref('Nuxt3 と Composition API の使い方')
+        const counter = ref(0)
+
+        const incrementCounter = () => {
+            counter.value++
+        }
+
+        return {
+            message,
+            counter,
+            incrementCounter
+        }
+    }
+}
+</script>
