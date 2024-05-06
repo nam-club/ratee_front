9b7c30f24d0255b0f1272fc6dc1e97abce60bd06
Takuma Aoki <aoking1213@gmail.com>
2024-05-06 15:01:48 +0900

    ランキングタブを縦表示に修正

--- components/organisms/QuestionnaireCard.vue
+++ components/organisms/QuestionnaireCard.vue
@@ -43,6 +43,50 @@
             </v-card>
         </v-col>
     </v-row>
+    <v-row v-else-if="!mobile && verticalDisplay">
+        <v-col cols="12" v-for="(questionnaire, index) in questionnaires" :key="index">
+            <v-card style="margin: 0 30%">
+                <v-container>
+                    <v-row class="justify-center">
+                        <v-col xs12 sm6 md6 align-self="center">
+                            <v-row no-gutters>
+                                <v-col cols="8">
+                                    <Msg fontWeight="normal" fontSize="1.5em" style="margin:2% 0">{{ questionnaire.content
+                                    }}
+                                    </Msg>
+                                </v-col>
+                                <v-col cols="4" class="text-end">
+                                    <nuxt-link :to="`/questionnaire/${questionnaire.id}`">
+                                        <Button :textColor="detailBtnTextColor" :variant="btnVariant">
+                                            {{ DETAIL_BUTTON }}
+                                        </Button>
+                                    </nuxt-link>
+                                </v-col>
+                            </v-row>
+                            <v-row no-gutters>
+                                <v-col cols="auto" v-for="(tag, i) in questionnaire.tags" :key="i">
+                                    <v-chip class="ma-1"
+                                        @click="() => { searchQuestionnaires(FORM_TAG_TEXT, tag); goToSearchTab(tag); }">
+                                        {{ tag }}
+                                    </v-chip>
+                                </v-col>
+                            </v-row>
+                            <v-container v-if="questionnaire.isAnswered === false">
+                                <AnswerBox :questionnaire="questionnaire" :answerQuestionnaire="answerQuestionnaire"
+                                    :answerSearchQuestionnaire="answerSearchQuestionnaire" :searchType="searchType"
+                                    :searchWord="searchWord" :searchCategory="searchCategory" />
+                            </v-container>
+                            <v-container v-if="questionnaire.isAnswered === true">
+                                <v-row>
+                                    <QuestionnaireBarChart :questionnaire="questionnaire" :options="options" />
+                                </v-row>
+                            </v-container>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card>
+        </v-col>
+    </v-row>
     <v-row v-else>
         <v-col cols="12" v-for="(questionnaire, index) in questionnaires" :key="index">
             <v-card>
