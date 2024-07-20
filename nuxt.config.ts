c18d60418771f7d8b193b0cf6ca453a9f5598c17
Takuma Aoki <aoking1213@gmail.com>
2024-07-20 16:06:09 +0900

    SEO対策

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,19 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'ratee -手軽にみんなの意見が見える投票サイト-',
+      meta: [
+        { name: 'description', content: '自分は多数派？少数派？世間の声が一目でわかる投票サイト。会員登録不要ですぐに遊べる！メジャーなお題からマイナーなお題まで気軽に聞いてみよう。' },
+        { name: 'keywords', content: 'ratee, 投票, アンケート, グラフ, みんなの声, 意見' },
+        { property: 'og:title', content: 'ratee -手軽にみんなの意見が見える投票サイト-' },
+        { property: 'og:description', content: '自分は多数派？少数派？世間の声が一目でわかる投票サイト。会員登録不要ですぐに遊べる！メジャーなお題からマイナーなお題まで気軽に聞いてみよう。' },
+        { property: 'og:image', content: '/logo.png' },
+        { property: 'og:url', content: 'https://ratee.nam-club.com/' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'ratee -手軽にみんなの意見が見える投票サイト-' },
+        { name: 'twitter:description', content: '自分は多数派？少数派？世間の声が一目でわかる投票サイト。会員登録不要ですぐに遊べる！メジャーなお題からマイナーなお題まで気軽に聞いてみよう。' },
+        { name: 'twitter:image', content: '/logo.png' }
+      ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=M+PLUS+1p&family=M+PLUS+Rounded+1c&display=swap' },
