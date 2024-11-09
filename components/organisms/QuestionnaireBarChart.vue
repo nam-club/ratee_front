0a69903c7d97ea5a61ac1f126919435acf2dacca
Takuma Aoki <aoking1213@gmail.com>
2024-11-09 17:10:13 +0900

    #80 棒グラフの大きさとデータラベルをパーセンテージ表示に変更する

--- components/organisms/QuestionnaireBarChart.vue
+++ components/organisms/QuestionnaireBarChart.vue
@@ -1,69 +1,122 @@
 <template>
-    <BarChart :data="chartData" :options="options" />
+  <BarChart :data="chartData" :options="options" />
 </template>
   
 <script>
-import { computed } from 'vue';
-
-import { mainTheme } from '@/helpers/themes'
-import BarChart from '@/components/molecules/BarChart.vue'; // Path to your BarChart component
+import { computed } from "vue";
+import BarChart from "@/components/molecules/BarChart.vue"; // Path to your BarChart component
 
 export default {
-    components: {
-        BarChart,
+  components: {
+    BarChart,
+  },
+  props: {
+    questionnaire: {
+      type: Object,
+      required: true,
     },
-    props: {
-        questionnaire: {
-            type: Object,
-            required: true,
+    options: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const chartOptions = computed(() => ({
+      ...props.options,
+      animation: false,
+      plugins: {
+        legend: {
+          display: false, // 凡例を非表示に設定
         },
-        options: {
-            type: Object,
-            required: true,
+        tooltip: {
+          callbacks: {
+            label: (context) => {
+              const index = context.dataIndex;
+              const choice = props.questionnaire.choices[index];
+              return `${choice.voteCount}人`;
+            },
+          },
         },
-    },
-    setup(props) {
-        const labels = computed(() => props.questionnaire.choices.map(choice => choice.name));
-        const dataset = computed(() => props.questionnaire.choices.map(choice => choice.voteCount));
+      },
+    }));
 
-        const backgroundColors = computed(() => {
-            return props.questionnaire.choices.map(choice => choice.isMyChoice ? '#7fbfff' : '#abb1b5');
-        });
+    const labels = computed(() =>
+      props.questionnaire.choices.map((choice) => choice.name)
+    );
 
-        const chartData = computed(() => ({
-            labels: labels.value,
-            datasets: [{
-                data: dataset.value,
-                backgroundColor: backgroundColors.value,
-                borderRadius: Number.MAX_VALUE,
-                datalabels: {
-                    color: (context) => {
-                        // データ値が棒グラフの幅より大きい場合は黒、そうでない場合は白
-                        const value = context.dataset.data[context.dataIndex];
-                        const meta = context.chart.getDatasetMeta(0);
-                        const w = meta.data[context.dataIndex].width;
-                        return context.chart.ctx.measureText(value).width > w ? '#000000' : '#FFFFFF';
-                    },
-                    anchor: (context) => {
-                        // データ値が棒グラフの幅より大きい場合は'end'、そうでない場合は'center'
-                        const value = context.dataset.data[context.dataIndex];
-                        const meta = context.chart.getDatasetMeta(0);
-                        const w = meta.data[context.dataIndex].width;
-                        return context.chart.ctx.measureText(value).width > w ? 'end' : 'center';
-                    },
-                    align: 'center',
-                    font: {
-                        size: 14,
-                        family: "'Kosugi Maru'",
-                    },
-                    formatter: (value) => value === 0 ? '' : value // 0の場合は空文字を返す
-                }
-            }],
-        }));
+    // 総投票数を計算
+    const totalVotes = computed(() =>
+      props.questionnaire.choices.reduce(
+        (sum, choice) => sum + choice.voteCount,
+        0
+      )
+    );
 
-        return {
-            chartData,
-        };
-    },
+    // パーセンテージのデータセットを計算
+    const dataset = computed(() =>
+      props.questionnaire.choices.map((choice) =>
+        totalVotes.value > 0
+          ? Math.round((choice.voteCount / totalVotes.value) * 100)
+          : 0
+      )
+    );
+
+    const backgroundColors = computed(() => {
+      return props.questionnaire.choices.map((choice) =>
+        choice.isMyChoice ? "#7fbfff" : "#abb1b5"
+      );
+    });
+
+    const chartData = computed(() => ({
+      labels: labels.value,
+      datasets: [
+        {
+          data: dataset.value,
+          backgroundColor: backgroundColors.value,
+          borderRadius: Number.MAX_VALUE,
+          datalabels: {
+            color: (context) => {
+              // データ値が棒グラフの幅より大きい場合は黒、そうでない場合は白
+              const value = context.dataset.data[context.dataIndex];
+              const meta = context.chart.getDatasetMeta(0);
+              const w = meta.data[context.dataIndex]?.width || 0;
+              console.log(w);
+              return w && context.chart.ctx.measureText(`${value}%`).width > w
+                ? "#000000"
+                : "#FFFFFF";
+            },
+            anchor: (context) => {
+              // データ値が棒グラフの幅より大きい場合は'end'、そうでない場合は'center'
+              const value = context.dataset.data[context.dataIndex];
+              const meta = context.chart.getDatasetMeta(0);
+              const w = meta.data[context.dataIndex]?.width || 0;
+              return w && context.chart.ctx.measureText(`${value}%`).width > w
+                ? "end"
+                : "center";
+            },
+            align: (context) => {
+              // データ値が棒グラフの幅より大きい場合は'end'、そうでない場合は'center'
+              const value = context.dataset.data[context.dataIndex];
+              const meta = context.chart.getDatasetMeta(0);
+              const w = meta.data[context.dataIndex]?.width || 0;
+              return w && context.chart.ctx.measureText(`${value}%`).width > w
+                ? "end"
+                : "center";
+            },
+            font: {
+              size: 14,
+              family: "'Kosugi Maru'",
+            },
+            formatter: (value) => (value === 0 ? "" : `${value}%`), // パーセント記号を追加
+          },
+        },
+      ],
+    }));
+
+    return {
+      chartData,
+      options: chartOptions,
+    };
+  },
 };
 </script>  
--- components/organisms/TimeChart.vue
+++ components/organisms/TimeChart.vue
@@ -1,87 +1,98 @@
 <template>
-    <LineChart :chartData="chartData" :chartOptions="options" />
+  <LineChart :chartData="chartData" :chartOptions="chartOptions" />
 </template>
 
 <script lang="ts">
-import { ref, watch, defineComponent } from 'vue';
-import LineChart from '@/components/molecules/LineChart.vue';
+import { ref, watch, defineComponent } from "vue";
+import LineChart from "@/components/molecules/LineChart.vue";
 
 export default defineComponent({
-    components: {
-        LineChart,
+  components: {
+    LineChart,
+  },
+  props: {
+    timeData: {
+      type: Object,
+      required: true,
     },
-    props: {
-        timeData: {
-            type: Object,
-            required: true,
-        },
-        options: {
-            type: Object,
-            required: true,
-        },
+    options: {
+      type: Object,
+      required: true,
     },
-    setup(props) {
-        const chartData = ref({ labels: [], datasets: [] });
+  },
+  setup(props) {
+    const chartData = ref({ labels: [], datasets: [] });
+    const chartOptions = computed(() => ({
+      ...props.options,
+      animation: false,
+    }));
 
-        const defaultColors = [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(199, 199, 199, 1)',
-            'rgba(83, 102, 255, 1)',
-            'rgba(220, 100, 200, 1)',
-            'rgba(101, 143, 143, 1)',
-        ];
+    const defaultColors = [
+      "rgba(255, 99, 132, 1)",
+      "rgba(54, 162, 235, 1)",
+      "rgba(255, 206, 86, 1)",
+      "rgba(75, 192, 192, 1)",
+      "rgba(153, 102, 255, 1)",
+      "rgba(255, 159, 64, 1)",
+      "rgba(199, 199, 199, 1)",
+      "rgba(83, 102, 255, 1)",
+      "rgba(220, 100, 200, 1)",
+      "rgba(101, 143, 143, 1)",
+    ];
 
-        const processData = (obj) => {
-            if (!obj || Object.keys(obj).length === 0) {
-                return;
-            }
-            const labels = new Set();
-            const datasets = [];
-            const dataPoints = Object.entries(obj.data);
-            const choices = Object.entries(obj.choices);
+    const processData = (obj) => {
+      if (!obj || Object.keys(obj).length === 0) {
+        return;
+      }
+      const labels = new Set();
+      const datasets = [];
+      const dataPoints = Object.entries(obj.data);
+      const choices = Object.entries(obj.choices);
 
-            choices.forEach(([key, value], index) => {
-                const colorIndex = index % defaultColors.length;
-                const borderColor = defaultColors[colorIndex];
-                datasets.push({
-                    label: value,
-                    data: [],
-                    borderColor: borderColor,
-                    backgroundColor: borderColor.replace('1)', '0.2)'),
-                    cubicInterpolationMode: 'monotone',
-                    tension: 0.4
-                });
-            });
-
-            dataPoints.forEach(([date, values]) => {
-                labels.add(date);
-
-                Object.entries(values).forEach(([key, value], index) => {
-                    const datasetIndex = choices.findIndex(([choiceKey]) => choiceKey === key);
-                    if (datasetIndex !== -1) {
-                        datasets[datasetIndex].data.push(value);
-                    }
-                });
-            });
-
-            chartData.value.labels = Array.from(labels);
-            chartData.value.datasets = datasets;
-        };
-
-        watch(() => props.timeData, (newVal) => {
-            processData(newVal);
-        }, {
-            immediate: true,
+      choices.forEach(([key, value], index) => {
+        const colorIndex = index % defaultColors.length;
+        const borderColor = defaultColors[colorIndex];
+        datasets.push({
+          label: value,
+          data: [],
+          borderColor: borderColor,
+          backgroundColor: borderColor.replace("1)", "0.2)"),
+          cubicInterpolationMode: "monotone",
+          tension: 0.4,
         });
+      });
 
-        return {
-            chartData,
-        };
-    },
+      dataPoints.forEach(([date, values]) => {
+        labels.add(date);
+
+        Object.entries(values).forEach(([key, value], index) => {
+          const datasetIndex = choices.findIndex(
+            ([choiceKey]) => choiceKey === key
+          );
+          if (datasetIndex !== -1) {
+            datasets[datasetIndex].data.push(value);
+          }
+        });
+      });
+
+      chartData.value.labels = Array.from(labels);
+      chartData.value.datasets = datasets;
+    };
+
+    watch(
+      () => props.timeData,
+      (newVal) => {
+        processData(newVal);
+      },
+      {
+        immediate: true,
+      }
+    );
+
+    return {
+      chartData,
+      chartOptions
+    };
+  },
 });
 </script>
